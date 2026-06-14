6b4ea67068936184780f7687f403d5e26bcf8747
Jennie <jennie.snyder@outlook.com>
2026-06-14 17:57:35 -0500

    Add quote.py to display a motivational quote
    This program prints a motivational quote and credits the original author.

---
+++ week-2/quote.py
@@ -0,0 +1,9 @@
+"""
+Author: Jennifer Snyder
+Date: June 14, 2026
+File Name: quote.py
+Description: This program prints a motivational quote and credits the original author.
+"""
+
+# Printing the selected quote and the author's name to the console
+print('"The only impossible journey is the one you never begin." - Tony Robbins')
